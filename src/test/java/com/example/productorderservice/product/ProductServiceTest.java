5ad7c4de629330139a0f5ec4e34cce33d69093c0
doxxx <doxxx93@gmail.com>
2023-01-30 17:45:53 +0900

    refactor: convert to spring boot test

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,28 +1,27 @@
 package com.example.productorderservice.product;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ProductServiceTest {
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-    private ProductRepository productRepository;
-
-    @BeforeEach
-    void setUp() {
-        productRepository = new ProductRepository();
-        productPort = new ProductAdapter(productRepository);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     @DisplayName("상품을 등록한다.")
     void addProduct() {
+        final AddProductRequest request = getAddProductRequest();
+
+        productService.addProduct(request);
+    }
+
+    private static AddProductRequest getAddProductRequest() {
         final String name = "상품명";
         final int price = 1000;
         final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
-        productService.addProduct(request);
+        return new AddProductRequest(name, price, discountPolicy);
     }
 }
