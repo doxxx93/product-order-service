41e1d6e5ce6bad96d59c9fba4a4c87949ee5f844
doxxx <doxxx93@gmail.com>
2023-02-01 13:22:25 +0900

    test: convert getProduct to api test

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -3,6 +3,9 @@ package com.example.productorderservice.product;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -16,5 +19,20 @@ class ProductApiTest extends ApiTest {
         final var response = ProductSteps.addProductRequest(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("상품을 조회한다.")
+    void getProduct() {
+        ProductSteps.addProductRequest(ProductSteps.getAddProductRequest());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 }
