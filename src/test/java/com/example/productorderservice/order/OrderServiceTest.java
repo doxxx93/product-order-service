39c9f97e34efd7517eafdd063edd6ba54a52376a
doxxx <doxxx93@gmail.com>
2023-02-01 19:35:22 +0900

    test: createOrder pojo

--- src/test/java/com/example/productorderservice/order/OrderServiceTest.java
+++ src/test/java/com/example/productorderservice/order/OrderServiceTest.java
@@ -1,15 +1,34 @@
 package com.example.productorderservice.order;
 
+import com.example.productorderservice.product.DiscountPolicy;
+import com.example.productorderservice.product.Product;
+import com.example.productorderservice.product.ProductRepository;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 class OrderServiceTest {
     private OrderService orderService;
+    private OrderPort orderPort;
+    private OrderRepository orderRepository;
 
     @BeforeEach
     void setUp() {
-        orderService = new OrderService();
+        orderRepository = new OrderRepository();
+        orderPort = new OrderPort() {
+            @Override
+            public Product getProductById(final Long productId) {
+                return new Product("상품명", 1000, DiscountPolicy.NONE);
+            }
+
+            @Override
+            public void save(final Order order) {
+                orderRepository.save(order);
+            }
+        };
+        orderService = new OrderService(orderPort);
     }
 
     @Test
@@ -21,8 +40,18 @@ class OrderServiceTest {
     }
 
     private class OrderService {
+        private final OrderPort orderPort;
+
+        private OrderService(final OrderPort orderPort) {
+            this.orderPort = orderPort;
+        }
+
         public void createOrder(final CreateOrderRequest request) {
-            throw new UnsupportedOperationException("Unsupported createOrder");
+            final Product product = orderPort.getProductById(request.productId());
+
+            final Order order = new Order(product, request.quantity());
+
+            orderPort.save(order);
         }
     }
 
@@ -31,6 +60,65 @@ class OrderServiceTest {
             Assert.notNull(productId, "상품 ID는 필수입니다.");
             Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
         }
+    }
 
+    private class OrderAdapter implements OrderPort{
+
+        private final ProductRepository productRepository;
+        private final OrderRepository orderRepository;
+
+        private OrderAdapter(final ProductRepository productRepository, final OrderRepository orderRepository) {
+            this.productRepository = productRepository;
+            this.orderRepository = orderRepository;
+        }
+
+        @Override
+        public Product getProductById(final Long productId) {
+            return productRepository.findById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+        }
+
+        @Override
+        public void save(final Order order) {
+            orderRepository.save(order);
+        }
+    }
+
+    private class Order {
+        private Long id;
+        private final Product product;
+        private final int quantity;
+
+
+        public Order(final Product product, final int quantity) {
+            Assert.notNull(product, "상품은 필수입니다.");
+            Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
+            this.product = product;
+            this.quantity = quantity;
+        }
+
+        public void assignId(final Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
+
+    private class OrderRepository {
+
+        private final Map<Long, Order> persistence = new HashMap<>();
+        private Long sequence = 0L;
+
+        public void save(final Order order) {
+            order.assignId(++sequence);
+            persistence.put(order.getId(), order);
+        }
+    }
+
+    private interface OrderPort {
+        Product getProductById(final Long productId);
+        void save(final Order order);
     }
 }
